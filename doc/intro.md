1b83e588535cd9daeebaf46e7a2cd520713cc018
Jason Bell <jason@mastodonc.com>
2018-05-25 16:55:57 +0100

    Project import

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to kixi.paloma.enrich
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/kixi/paloma/enrich.clj
@@ -0,0 +1,7 @@
+(ns kixi.paloma.enrich
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
